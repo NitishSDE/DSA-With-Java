7f77a87f2525fac7de3dade0d00c9e303516290a
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 08:15:51 +0530

    002.Output In Java

---
+++ 002.Output In Java/Main2.java
@@ -0,0 +1,8 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.print("Hellow World");
+        System.out.print("Hellow World");
+        System.out.print("Hellow World");
+    }
+}
---
+++ 002.Output In Java/Main3.java
@@ -0,0 +1,8 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+        System.out.print("Hellow World\n");
+        System.out.print("Hellow World\n");
+        System.out.print("Hellow World\n");
+    }
+}
