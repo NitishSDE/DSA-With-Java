ade92364f8a3a959f3d38689a4018f2b9d4dcdfb
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 23:53:50 +0530

    012.Type Conversion

---
+++ 012.Type Conversion/Main.java
@@ -0,0 +1,17 @@
+/*
+ Type conversion happens when
+ 1.type compatible
+ 2.destination type > source type
+
+ byte -> short -> int -> float -> long -> double 
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        float a = 12f;
+        int b = a;
+
+
+    }
+}
---
+++ 012.Type Conversion/Main2.java
@@ -0,0 +1,8 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        int a = 12;
+        float b = a; // It Allows, because java will convert from int float.
+        System.out.println(b); 
+    }
+}
