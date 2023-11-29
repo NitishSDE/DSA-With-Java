95de22b619f8a96ab487bec3740eb16ef76d2f97
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 08:37:16 +0530

    004.Variables In Java

---
+++ 004.Variables In Java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 87;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Nitish Kuamr";
+        System.out.println(name);
+    }
+}
