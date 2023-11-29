c15c8694273e01d57c49c52ebb5d5cfe35d17974
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 09:00:34 +0530

    005.Data Types in Java

---
+++ 005.Data Types in Java/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte num1 = 12;
+        System.out.println(num1);
+
+        short num2 = 345;
+        System.out.println(num2);
+
+        int num3 = 1567;
+        System.out.println(num3);
+
+        long num4 = 123456779L;
+        System.out.println(num4);
+
+        char letter = 'A';
+        System.out.println(letter);
+
+        boolean isMale = false;
+        System.out.println(isMale);
+
+        float a = 12.34f;
+        System.out.println(a);
+
+        double b = 45.78;
+        System.out.println(b);
+
+        double c = 45.78d;
+        System.out.println(b);
+
+
+    }
+}
