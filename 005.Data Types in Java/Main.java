f28849b724d2b7075c093b59410a44f025897f53
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 09:02:04 +0530

    005.Data Types in Java

--- 005.Data Types in Java/Main.java
+++ 005.Data Types in Java/Main.java
@@ -1,28 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        byte num1 = 12;
+        byte num1 = 12; // Size = 1 byte
         System.out.println(num1);
 
-        short num2 = 345;
+        short num2 = 345; // Size = 2 bytes
         System.out.println(num2);
 
-        int num3 = 1567;
+        int num3 = 1567;  // Size = 4 bytes
         System.out.println(num3);
 
-        long num4 = 123456779L;
+        long num4 = 123456779L;  // Size = 8 bytes
         System.out.println(num4);
 
-        char letter = 'A';
-        System.out.println(letter);
-
-        boolean isMale = false;
+        boolean isMale = false; // Size = 1 byte
         System.out.println(isMale);
 
-        float a = 12.34f;
+        char letter = 'A';  // Size = 2 bytes
+        System.out.println(letter);
+
+        float a = 12.34f; // Size = 4 bytes
         System.out.println(a);
 
-        double b = 45.78;
+        double b = 45.78;  // Size = 8 bytes;
         System.out.println(b);
 
         double c = 45.78d;
