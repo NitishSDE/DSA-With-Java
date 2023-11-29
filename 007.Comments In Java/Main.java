1992df43424769804af4936ef299aff937581e19
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 12:29:58 +0530

    007.Comments In Java

---
+++ 007.Comments In Java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is Single line comments");
+        // Single Line comments.
+        System.out.println("This is Multiline line comments");
+        /*
+         This
+         is 
+         Multiline
+         Comments
+         */
+
+    }
+}
