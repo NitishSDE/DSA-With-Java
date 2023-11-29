e5f86de515e7b0af1d403d014c1fd79ed9ed8f86
NitishSDE <nkroy1993@yahoo.com>
2023-11-29 22:55:06 +0530

    008.Taking Input In Java

---
+++ 008.Taking Input In Java/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        System.out.println(firstName);
+
+        System.out.println("Enter full name");
+        String fullName = sc.nextLine();
+        System.out.println(fullName);
+
+        System.out.println("Enter a integer");
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        System.out.println("Enter a Double");
+        double b = sc.nextDouble();
+        System.out.println(b);
+
+        
+        
+    }
+}
