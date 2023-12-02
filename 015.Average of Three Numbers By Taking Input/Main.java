470f0bfd0403cccd465e68bb3912219aea28aba2
NitishSDE <nkroy1993@yahoo.com>
2023-12-02 19:41:12 +0530

    015.Average of Three Numbers By Taking Input

---
+++ 015.Average of Three Numbers By Taking Input/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Three Numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        double avg = (a + b + c)/3;
+        System.out.println("Avearge of Three Number is "+ avg);
+    }
+}
