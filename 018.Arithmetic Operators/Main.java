88d05fb7acddc39c38208846a7f1571f2f3db5d7
NitishSDE <nkroy1993@yahoo.com>
2023-12-02 22:25:50 +0530

    018.Arithmetic Operators

---
+++ 018.Arithmetic Operators/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 5;
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Subtarction: "
+        + (a-b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: "+ (a/b));
+        System.out.println("Remainder: "+ (a%b));
+    }
+}
