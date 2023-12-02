358bbaa5a5fd553073677ad3315d7e3f6ab4a495
NitishSDE <nkroy1993@yahoo.com>
2023-12-02 23:31:58 +0530

    023.If Else Introduction

---
+++ 023.If Else Introduction/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 19;
+        if(age > 18){
+            System.out.println("You Can Drive");
+        }
+        else{
+            System.out.println("You Can't Drive");
+        }
+    }
+}
