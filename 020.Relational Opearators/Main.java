bf3939dc2ec7653cc4e75d725256d070b478bb6d
NitishSDE <nkroy1993@yahoo.com>
2023-12-02 22:46:58 +0530

    020.Relational Opearators

---
+++ 020.Relational Opearators/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int A = 19;
+        int B = 23;
+        System.out.println(A > B);
+        System.out.println(A < B);
+        System.out.println(A >= B);
+        System.out.println(A <= B);
+        System.out.println(A == B);
+        System.out.println(A != B);
+    }
+}
