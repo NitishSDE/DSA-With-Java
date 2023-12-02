c6a305fa274bf326e960be98cbd259eebb86c7bd
NitishSDE <nkroy1993@yahoo.com>
2023-12-02 23:16:57 +0530

    022.Assignment Operators

---
+++ 022.Assignment Operators/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10; // Assignment Opearetors
+        a += 10; // a = a+10
+        System.out.println(a);
+
+        int b = 12;
+        b -= 8; // b = b-8
+        System.out.println(b);
+
+        int c = 15;
+        c *= 3; // c  = c*3
+        System.out.println(c);
+
+        int d = 124;
+        d /= 5; // d = d/5
+        System.out.println(d);
+
+        int e = 123;
+        e %= 5; //d = d%5
+        System.out.println(e);
+
+
+
+    }
+}
