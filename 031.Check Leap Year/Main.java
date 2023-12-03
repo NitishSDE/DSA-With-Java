1314473b064c944ca671981d85425de1744ee314
NitishSDE <nkroy1993@yahoo.com>
2023-12-03 20:45:56 +0530

    031.Check Leap Year

---
+++ 031.Check Leap Year/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int year = 2000;
+        if(year % 400 == 0){
+            System.out.println("Leap Year");
+        }
+        else if((year % 4 == 0) && (year % 100 != 0)){
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("Not A Leap Year");
+        }
+    }
+}
