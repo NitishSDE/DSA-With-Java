c0b95592b44725ae6db609fff6869dac62bf4519
NitishSDE <nkroy1993@yahoo.com>
2023-12-03 11:42:37 +0530

    024.Print The Largest of Two

---
+++ 024.Print The Largest of Two/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 78;
+        int b = 78;
+        if(a > b){
+            System.out.println(a+ " is greater than "+ b);
+        }
+        else if (a < b){
+            System.out.println(b+ " is greater than "+ a);
+        }
+        else{
+            System.out.println(a+ " is equal to "+ b);
+        }
+    }
+}
