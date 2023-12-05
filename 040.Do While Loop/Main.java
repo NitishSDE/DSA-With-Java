a50605b89dcbbcac636806b4b59f1182ba62e0f2
NitishSDE <nkroy1993@yahoo.com>
2023-12-05 22:15:26 +0530

    040.Do While Loop

---
+++ 040.Do While Loop/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        // Follwoing Code print 1 times hello world 
+        /*do{
+            System.out.println("Hello World!");
+            i++;
+        }while(i <1);*/
+
+        do{
+            System.out.println("Hello World!");
+            i++;
+        }while(i <= 10);
+
+
+    }
+}
