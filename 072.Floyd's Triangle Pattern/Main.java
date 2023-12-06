bae29043ff2796c8b328c976d5ded347a1f6edc6
NitishSDE <nkroy1993@yahoo.com>
2023-12-06 22:21:16 +0530

    072.Floyd's Triangle Pattern

---
+++ 072.Floyd's Triangle Pattern/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void floyds_triangle_pattern(int n){
+        int counter = 1;
+        for(int i=1; i<=n; i++){
+           for(int j=1; j<=i; j++){
+            System.out.print(counter +" ");
+            counter++;
+           }
+           System.out.println();
+        }
+       
+    }
+    public static void main(String[] args) {
+        floyds_triangle_pattern(5);
+        floyds_triangle_pattern(12);
+    }
+}
