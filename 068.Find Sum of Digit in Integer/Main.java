7cc2040e46cdcd66e0fdf9c9d48144010db6a119
NitishSDE <nkroy1993@yahoo.com>
2023-12-06 17:02:39 +0530

    068.Find Sum of Digit in Integer

---
+++ 068.Find Sum of Digit in Integer/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static int findSum(int n){
+        int sum = 0;
+        while (n > 0) {
+            int lastDigit = n%10;
+            sum = sum +lastDigit;
+            n = n/10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findSum(123));
+    }
+}
