488e720b0cab58291a420012082445e0dad43cbd
NitishSDE <nkroy1993@yahoo.com>
2023-12-06 13:06:05 +0530

    061.Prime Number Checking by Using Function

---
+++ 061.Prime Number Checking by Using Function/Main1.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        else{
+            for(int i=2; i<=Math.sqrt(n); i++){
+                if(n%i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(13));
+        System.out.println(isPrime(4));
+    }
+}
