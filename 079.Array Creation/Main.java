229e4b4555c99aea0b4480b3d8e234f81212225a
NitishSDE <nkroy1993@yahoo.com>
2023-12-07 09:02:29 +0530

    079.Array Creation

---
+++ 079.Array Creation/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int numbers[] ={1,2,3,4,5,6};
+        String name[] = new String[50];
+        String fruits[] = {"Apple", "Mango", "Orange"}; 
+    }
+}
